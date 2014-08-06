ce292724ded82bd528ba257e96fe5d51dfaa04e0
[dafei] <[dafei@16fan.com]>
2014-08-06 14:30:56 +0800

    后台增加清除缓存工具

--- protected/modules/admin/controllers/ToolsController.php
+++ protected/modules/admin/controllers/ToolsController.php
@@ -2,17 +2,44 @@
 
 class ToolsController extends H {
 
-    public $layout = 'admin';
-    
-    public function actionIndex() {
-        $type=zmf::filterInput($_GET['type'],'t',1);
-        if($type=='db'){
-            $this->redirect(array('db/index'));
-        }
-        $data=array(
-            'type'=>$type,
-        );
-        $this->render('index',$data);
+  public $layout = 'admin';
+
+  public function actionIndex() {
+    $type = zmf::filterInput($_GET['type'], 't', 1);
+    if ($type == 'db') {
+      $this->redirect(array('db/index'));
     }
+    $data = array(
+        'type' => $type,
+    );
+    $this->render('index', $data);
+  }
+
+  public function actionFunc() {
+    $type = zmf::filterInput($_GET['type'], 't', 1);
+    if (!$type || !in_array($type, array('assets', 'runtime', 'config'))) {
+      $this->message(0, '操作有误，不允许的操作类型', Yii::app()->createUrl('admin/tools/index'));
+    }
+    switch ($type) {
+      case 'assets':
+        $dir = Yii::app()->basePath . '/../assets/';
+        header("Content-type:text/html;charset=UTF-8");
+        zmf::destory($dir); //script
+        $this->message('script', '删除成功', Yii::app()->createUrl('admin/tools/index'));
+        break;
+      case 'runtime':
+        $dir = Yii::app()->basePath . '/runtime/';
+        header("Content-type:text/html;charset=UTF-8");
+        zmf::destory($dir);
+        $this->message('script', '删除成功', Yii::app()->createUrl('admin/tools/index'));
+        break;
+      case 'config':
+        tools::writeSet(array());
+        $this->message('script', '删除成功', Yii::app()->createUrl('admin/tools/index'));
+        break;
+      default:
+        $this->message(0, '操作有误，不允许的操作类型', Yii::app()->createUrl('admin/tools/index'));
+    }
+  }
 
 }
--- protected/modules/admin/views/tools/index.php
+++ protected/modules/admin/views/tools/index.php
@@ -1,15 +1,21 @@
 <h3>缓存管理</h3>
 <table class="table table-hover table-condensed">
     <tr>
-        <td>清理assets</td>
+        <td>
+        	<?php echo CHtml::link('清理assets',array('tools/func','type'=>'assets'));?>
+        	<span class="help-block">清理前端缓存的CSS、JS等</span>
+        </td>
     </tr>
     <tr>
-        <td>清理runtime</td>
+        <td>
+        	<?php echo CHtml::link('清理runtime',array('tools/func','type'=>'runtime'));?>
+        	<span class="help-block">清理缓存在本地的数据</span>
+        </td>
     </tr>
     <tr>
-        <td>系统配置</td>
-    </tr>
-    <tr>
-        <td>用户配置</td>
+        <td>
+        	<?php echo CHtml::link('系统配置',array('tools/func','type'=>'config'));?>
+        	<span class="help-block">重置系统设置</span>
+        	</td>
     </tr>
 </table>
