bb70ab0665fc40dad3d868cc364bc01d8ae75cfa
afei <no-reply@newsoul.cn>
2014-08-17 14:15:49 +0800

    完善企业列表

--- latest/protected/modules/admin/views/users/updateCredit.php
+++
@@ -1,8 +0,0 @@
-<?php $form=$this->beginWidget('CActiveForm',array('id'=>'xform','htmlOptions'=>array('name'=>'xform'))); ?>
-<div class="form-group">
-    <?php echo $form->labelEx($model,'order'); ?>
-    <?php echo $form->textField($model,'order',array('size'=>11,'maxlength'=>11,'class'=>'form-control')); ?>
-    <?php echo $form->error($model,'order'); ?>
-</div>
-<?php echo CHtml::submitButton('排序',array('class'=>'btn btn-default','name'=>'')); ?>
-<?php $form=$this->endWidget(); ?>
