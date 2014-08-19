e7b3298a8727782516bf737732b3e65fa448b4fd
[dafei] <[dafei@16fan.com]>
2014-08-19 16:08:06 +0800

    修改企业

--- protected/views/module/qiye.php
+++ protected/views/module/qiye.php
@@ -9,6 +9,7 @@ if($this->inMobile){
 $users=Users::getExhibition('producer');
 if(!empty($users)){
     foreach($users as $uid){
+      $uid=$uid['uid'];
         echo '<div class="'.$list_colnum.' col-xs-'.$list_colnum.'">'.zmf::avatar($uid,'small').CHtml::link(Users::getUserInfo($uid,'truename'),array('mobile/index','uid'=>$uid),array('target'=>'_blank')).zmf::creditIcon($uid).'</div>';
     }
 }
