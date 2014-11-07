9bf8838533fe44aa088eab5ae9b2500d897ea71a
dafei <dafei@16fan.com>
2014-11-07 17:35:42 +0800

    index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 ini_set('memory_limit', '128M'); 
+error_reporting(E_ERROR);
 $yii=dirname(__FILE__).'/../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG',true);
